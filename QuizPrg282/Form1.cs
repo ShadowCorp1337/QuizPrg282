b661f5a7f1977384501e76ae86f3e68d05f493ff
ShadowCorp1337 <ShadowCorpmf@gmail.com>
2021-10-04 13:22:22 +0200

    Added Components

--- QuizPrg282/Form1.cs
+++ QuizPrg282/Form1.cs
@@ -16,5 +16,15 @@ namespace QuizPrg282
         {
             InitializeComponent();
         }
+
+        private void btnReadFile_Click(object sender, EventArgs e)
+        {
+            ;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
